78a8343cbbcf64cc254d1f1bc8d8900e8fbaac05
Akhil <akhil9tiet@gmail.com>
2020-01-11 18:08:40 -0800

    feat: handle empty products and delete a product

--- api/routes/products.js
+++ api/routes/products.js
@@ -9,7 +9,11 @@ router.get('/', (req, res, next) => {
 		.exec()
 		.then((docs) => {
 			console.log(docs);
-			res.status(200).json(docs);
+			if (docs.length > 0) { //check to prevent return of an empty products array
+				res.status(200).json(docs);
+			} else {
+				res.status(404).json({ message: 'No entries found' });
+			}
 		})
 		.catch((err) => {
 			console.log(err);
@@ -73,9 +77,16 @@ router.patch('/:productId', (req, res, next) => {
 });
 
 router.delete('/:productId', (req, res, next) => {
-	res.status(200).json({
-		message: 'Deleted product!'
-	});
+	const id = req.params.productId;
+	Product.remove({ _id: id })
+		.exec()
+		.then((result) => {
+			res.status(200).json(result);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ error: err });
+		});
 });
 
 module.exports = router;
