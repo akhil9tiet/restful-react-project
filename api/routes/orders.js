6414ad9fd96d8ce0f5b4b0cbd104dd39025113ce
Akhil <akhil9tiet@gmail.com>
2020-01-12 13:34:45 -0800

    feat: fetch all orders in  beautified way

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,9 +5,28 @@ const mongoose = require('mongoose');
 const Order = require('../models/order');
 
 router.get('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'Orders was fetched'
-	});
+	Order.find()
+		.select('product quantity _id')
+		.exec()
+		.then((docs) => {
+			res.status(200).json({
+				count: docs.length,
+				orders: docs.map((doc) => {
+					return {
+						_id: doc._id,
+						product: doc.product,
+						quantity: doc.quantity,
+						request: {
+							type: 'GET',
+							url: 'http://localhost:300/orders/' + doc._id
+						}
+					};
+				})
+			});
+		})
+		.catch((err) => {
+			res.status(500).json({ error: err });
+		});
 });
 
 router.post('/', (req, res, next) => {
@@ -30,7 +49,7 @@ router.post('/', (req, res, next) => {
 
 router.get('/:orderId', (req, res, next) => {
 	res.status(200).json({
-		message: 'Orders details',
+		message: 'Order was fetched',
 		orderId: req.params.orderId
 	});
 });
